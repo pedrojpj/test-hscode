7c4d0751684e6d5914ec7bdb133eba54e2643771
pedrojose.pena <pedrojose.pena@i3television.es>
2018-03-07 12:38:44 +0100

    (Bugfix) Api

--- src/utils/apiHS.js
+++ src/utils/apiHS.js
@@ -1,6 +1,6 @@
 import codes from './data/hscode.json';
 
-export const getChapters = () => codes.results.filter(item => item.id.length === 2);
+export const getChapters = () => codes.results.filter(item => item.parent === 'TOTAL');
 export const getHeadings = chapter =>
   codes.results.filter(item => item.parent === chapter).map(item => ({
     ...item,
